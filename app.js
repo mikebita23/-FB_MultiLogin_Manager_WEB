6c1c8fc10d11290bded22b48177a29cacda8d763
Nidal El Fengaoui <nidal.fengaoui@gmail.com>
2021-01-11 22:20:18 +0100

    setup structure for back-end

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+global.__views = path.resolve(__dirname, './public/views')
+
+var indexRouter = require('./routes/index');
+var adminRouter = require('./routes/admin')
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/admin', adminRouter)
+
+module.exports = app;
