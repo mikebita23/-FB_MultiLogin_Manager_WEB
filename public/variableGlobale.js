b6ad841d2351197be0923c91e158806ea415e24c
dani03 <lesamessipa@gmail.com>
2021-02-04 00:26:29 +0100

    injection des données dans le modal pour utilisateurs

--- public/variableGlobale.js
+++ public/variableGlobale.js
@@ -1,4 +1,4 @@
 var application = angular.module('app', ['ngRoute']);
 const API_URL_D = "http://api.infinite-scale.fr";
 const API_URL = "http://localhost:3003";
-let token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImlicmFAZ21haWwuY29tIiwidXNlcklkIjoxOSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjEyMzY1OTM4LCJleHAiOjE2MTIzNjk1Mzh9.HlFIbAMe-SB8HBFBPb5a0sW3XESfZvnXv7BdBCIdmQU'
+let token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhdmlkQGdtYWlsLmNvbSIsInVzZXJJZCI6OSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjEyMzkyMjQ2LCJleHAiOjE2MTIzOTU4NDZ9.cq7WpttEfErW337Og3BdE5ejmUhGaR4Uvk6Q4SrOReM'
