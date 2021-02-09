7e3a8025d57055b8d0b8dd5a8a560bed4b4182d3
dani03 <lesamessipa@gmail.com>
2021-02-09 11:52:16 +0100

    mise en page des forfaits sur le site

--- public/variableGlobale.js
+++ public/variableGlobale.js
@@ -2,4 +2,4 @@
 var application = angular.module('app', ['ngRoute']);
 const API_URL_D = "http://api.infinite-scale.fr";
 const API_URL = "http://localhost:3003";
-var  token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhdmlkQGdtYWlsLmNvbSIsInVzZXJJZCI6OSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjEyNzkyOTA3LCJleHAiOjE2MTI3OTY1MDd9.zaLs8s9meFOSG-TZncq7V-_aICVbyljJ-6iiHjnN-L8'
+var  token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhdmlkQGdtYWlsLmNvbSIsInVzZXJJZCI6OSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjEyODY3NjI1LCJleHAiOjE2MTI4NzEyMjV9.XWUZnriF298ryb3qDSsh6Awlb7f3LWhF6tnp8qIwjuo'
