93b44b465227ecfcb5c31ae5dcc689d77c5532d2
dani03 <lesamessipa@gmail.com>
2021-02-10 15:24:08 +0100

    some bug fix from modal

--- public/variableGlobale.js
+++ public/variableGlobale.js
@@ -2,4 +2,4 @@
 var application = angular.module('app', ['ngRoute']);
 const API_URL_D = "http://api.infinite-scale.fr";
 const API_URL = "http://localhost:3003";
-var  token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhdmlkQGdtYWlsLmNvbSIsInVzZXJJZCI6OSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjEyODgyMjMwLCJleHAiOjE2MTI4ODU4MzB9.vcs-wd6SR1AIz21w3MqsTKBihrhFuPiwOqJ_V7ZaLSE'
+var  token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhdmlkQGdtYWlsLmNvbSIsInVzZXJJZCI6OSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjEyOTYzNzE0LCJleHAiOjE2MTI5NjczMTR9._sD1-MHRK2MsiQuFJvpNh33snhuWIyj_ph9BDyq4Kns'
