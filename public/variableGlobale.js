6841a9975551d058ddf862f4110df1e27022d167
GLS 02 <traoreibrahima2708>
2021-02-24 09:28:32 +0100

    Correction variable globaale

--- public/variableGlobale.js
+++ public/variableGlobale.js
@@ -2,5 +2,5 @@
 var application = angular.module('app', ['ngRoute']);
 const API_URL_D = "http://api.infinite-scale.fr";
 const API_URL = "http://localhost:3003";
-let token =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGRhbmkuY29tIiwidXNlcklkIjo3LCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2MTQwNjc1MDYsImV4cCI6MTYxNDA3MTEwNn0.w9GjFQFbw6ixWHcpuXrVObcQ5MNNyuuO6i71cUlHUpE"
+let token =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGRhbmkuY29tIiwidXNlcklkIjo3LCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2MTQwODMwMDEsImV4cCI6MTYxNDA4NjYwMX0.gwmJWyl2EJG3V0Hr6J9mgNDB-Ky0l0UIiumCMsAYOVA"
 
