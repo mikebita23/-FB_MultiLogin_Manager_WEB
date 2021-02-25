f17878a93444220bced95872fd81087af65a8505
dani03 <lesamessipa@gmail.com>
2021-02-25 10:47:00 +0100

    redirect when token expire

--- public/variableGlobale.js
+++ public/variableGlobale.js
@@ -9,4 +9,12 @@ let endPoint = URL[URL.length - 1 ]
 if(endPoint !== "login"){
     var token =  document.cookie
 }
-
+// regarde si le token est toujours valide
+function isValidToken(token){
+    curentTime = Math.floor(Date.now() / 1000);
+    console.log(Date.now() /1000);
+    console.log("the token", token );
+    console.log("voici le temps courant",curentTime);
+     return (token >= curentTime);
+}
+console.log(isValidToken(token));
